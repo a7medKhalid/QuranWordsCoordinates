d660d6b21ee8df00d0d3a20b7fb87f70e66b1393
a7medKhalid <69413330+a7medKhalid@users.noreply.github.com>
2023-02-22 21:12:28 +0300

    idk

--- gqOCR.py
+++ gqOCR.py
@@ -36,7 +36,7 @@ conn = sqlite3.connect('quranlines.db')
 c = conn.cursor()
 
 # create table
-c.execute('''CREATE TABLE words (page_number integer, line_number integer, word text, bounding_box text)''')
+c.execute('''CREATE TABLE IF NOT EXISTS words (page_number integer, line_number integer, word text, bound1 text, bound2 text, bound3 text, bound4 text, error boolean)''')
 
     
 dir_name = 'quranlines'
@@ -45,6 +45,40 @@ dir_name = 'quranlines'
 for p in range(1, 604):
 
     page_number = p
+
+    # https://api.quran.com/api/v4/verses/by_page/1?language=en&words=true&page=232&per_page=10
+
+    # get expected line words count from quran.com
+    import requests
+    url = 'https://api.quran.com/api/v4/verses/by_page/'+ str(page_number) + '?language=en&words=true'
+    r = requests.get(url)
+    data = r.json()
+    verses = data['verses']
+    lines = []
+
+    # CP:numbers of lines in a page is not always the same 
+    line_counter = verses[0]['words'][0]['line_number']
+    print(line_counter)
+    for verse in verses:
+        words = verse['words']
+        line_words = []
+        for word in words:
+
+            if word['line_number'] > line_counter:
+                lines.append(line_words)
+                line_words = []
+                line_counter += 1
+            
+            line_words.append(word)
+
+    print(lines)
+    print(len(lines))
+    # exit application
+    exit()
+
+
+
+
 
     # loop through lines
     for l in range(1, 15):
@@ -57,13 +91,13 @@ for p in range(1, 604):
         # detect text
         texts = detect_text(file_name)
 
-        # get text
-        text = texts[0].description
+        
 
-        # print text
-        print(text)
+
+
 
         # save text
+
 
         
 
